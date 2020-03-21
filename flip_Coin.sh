f0adef91a938fe832b8208faf9c4afbf970182bd
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-21 21:31:13 +0530

    Added:-to store doublet combination

--- flip_Coin.sh
+++ flip_Coin.sh
@@ -1,21 +1,38 @@
- #!/bin/bash -x
-echo "welcome to flipCoin simulator game"
-declare -A  singletDictionary
+#!/bin/bash -x
+declare -A  coin
+declare -A  pertoss
+echo "welcome to flip coin games"
+function flip_coin()
+{
+  for (( i=1; i<=no_of_toss; i++ ))
+  do
+         coinface=""
+     for (( j=1; j<=no_of_coins; j++ ))
+     do
+         toss=$((RANDOM%2))
+         if [ $toss -eq 0 ]
+         then
+                coinface+=head
+         else
+                coinface+=tail
+         fi
+     done
+        echo $coinface
+        coin[$coinface]=$((${coin[$coinface]}+1))
+        per $coinface
+  done
+      echo "${!coin[@]}"
+      echo "${coin[@]}"
+}
 
-for (( i=0; i<9; i++ ))
-do
-toss=$((RANDOM%2))
-if [ $toss -eq 0 ]
-then
-   singletDictionary[head]=$((${singletDictionary[head]}+1))
-else
-   singletDictionary[tail]=$((${singletDictionary[tail]}+1)) 
-fi
-done
-    echo "${singletDictionary[@]}"
-    echo "${!singletDictionary[@]}"
+function per()
+{
+       per=$((${coin[$coinface]}*100/$no_of_toss))
+       pertoss[$coinface]=$per
+       echo "key ${!pertoss[@]}"
+       echo "pecent ${pertoss[@]}"
+}
 
-headper=$((${singletDictionary[head]}*100/i))
-tailper=$((${singletDictionary[tail]}*100/i))
-echo $tailper
-echo $headper
+read -p "enter the no of toss" no_of_toss
+read -p "enter the no of coins" no_of_coins
+"$( flip_coin $(($no_of_toss,$no_of_coins)) )"
